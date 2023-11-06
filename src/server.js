a88254abdd9e25e178aca1e9bb18b5796320dddd
Your Name <you@example.com>
2023-11-06 17:13:13 +0000

    params added

--- src/server.js
+++ src/server.js
@@ -50,7 +50,6 @@ app.get("/getBooks", (req, res) => {
 })
 
 
-
 app.post("/addBook", (req, res) =>  {
     // Fake database
     const books = [
@@ -79,3 +78,51 @@ app.post("/addBook", (req, res) =>  {
     res.json(sucessResponse)
     console.log(books)
 })
+
+
+app.get("/findBook/:bookTitle", (req, res) => {
+    // fake database
+    const books = [
+        {
+            title: "Book 1",
+            author: "Author 1",
+            genre: "Action"
+        },
+        {
+            title: "Book 2",
+            author: "Author 2",
+            genre: "Fantasy",
+        },
+        {
+            title: "Book 3",
+            author: "Author 3",
+            genre: "Comedy",
+        }
+    ]
+
+    let title = req.params["bookTitle"]
+    console.log(title)
+
+    // find and match the book in fake database
+    let match = books.find(m => m.title === title)
+    console.log(match)
+
+    if (match) {
+        let sucessResponse = {
+            message: "Success, book found",
+            book: match
+        }
+        res.status(200).json(sucessResponse)
+    } else {
+
+        let errorResponse = {
+            message: "Sorry book not found"
+        }
+        res.status(501).json(errorResponse)
+    }
+
+    // search the fake database above using the book title passed in req.params
+    // if the book is found send it out in a response
+    // else if the book isn't found send a response saying book not found
+})
+
